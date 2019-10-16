476b750bdb1502345ec8fec6395c7102ddeac2a8
edwinisnow <edwin04@gmail.com>
2019-10-16 16:17:28 +0100

    initial commit

---
+++ src/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navigation() {
+    return (
+        <nav>
+            <h3>Logo</h3>
+            <ul>
+                <li>About</li>
+                <li>Shop</li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navigation
