06d77bbee9d899f7e8fa223eb6179ec381f3517b
edwinisnow <edwin04@gmail.com>
2019-10-18 00:05:19 +0100

    navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Navigation() {
+    const navStyle = {
+        color: 'white'
+    }
     return (
         <nav>
             <h3>Logo</h3>
             <ul className="nav-links">
+            <Link style={navStyle} to='/about'> 
                 <li>About</li>
+            </Link>
+            <Link style={navStyle} to='/shop'>
                 <li>Shop</li>
+            </Link>
             </ul>
         </nav>
     )
