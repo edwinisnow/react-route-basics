5ed70ae41a227ac9e3f6ca51bb2b0bac4ab1ec38
edwinisnow <edwin04@gmail.com>
2019-10-18 00:51:06 +0100

    Dynamic Route

--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom';
 function Shop() {
+
+    useEffect(()=>{
+        fetchItems()
+    },[])
+
+    const [items, setItem] = useState([]);
+
+    const fetchItems = async()=>{
+        const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
+        const items = await data.json()
+        console.log(items);
+
+        setItem(items.items);
+    }
     return (
         <div>
             Shop Page
+            {items.map(item=> <h4 key={item.itemid}>
+            <Link to={`/shop/${item.itemid}`}>{item.name}</Link>
+                </h4>)}
         </div>
     )
-}
+} 
 
 export default Shop
